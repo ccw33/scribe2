93776c7fdbb6545fb9a8afc25733ca33729803df
ccw <952424500@qq.com>
2018-08-07 11:53:05 +0800

    完成基本的client和server

---
+++ Server/Service/fanqiang_ip_service.py
@@ -0,0 +1,44 @@
+#encoding:utf-8
+
+import math
+import random
+
+import requests
+
+from model.fangqiang import ip
+from Utils import scribe_utils
+
+fanqiang = ip.Fanqiang()
+
+def get_random_ip_port_dict():
+    '''
+    获取一个有用的ip_port_dict
+    :return:
+    '''
+    ip_list = list(fanqiang.query())
+    ip_port_dict = ip_list[math.floor(random.random() * len(ip_list))]
+    try:
+        test_proxy(ip_port_dict)
+        return ip_port_dict
+    except requests.exceptions.ConnectionError or requests.exceptions.ReadTimeout or requests.exceptions.SSLError as e:  # request 访问错误
+        fanqiang.delete({'ip_with_port':ip_port_dict['ip_with_port']})
+        return get_random_ip_port_dict()
+
+
+def test_proxy(ip_port_dict):
+    '''
+    测试ip代理
+    :param ip_port_dict: 包含ip port等数据的dict对象
+    :type ip_port_dict: dict
+    :return:use_time 用时，秒为单位。
+    '''
+    ip_with_port = ip_port_dict['ip_with_port']
+    proxy_type = ip_port_dict['proxy_type']
+    resp = requests.get('https://www.baidu.com/', headers=scribe_utils.headers,
+                        proxies={'http': proxy_type + (
+                            'h' if proxy_type == 'socks5' else '') + '://' + ip_with_port,
+                                 'https': proxy_type + (
+                                     'h' if proxy_type == 'socks5' else '') + '://' + ip_with_port},
+                        timeout=10)
+    use_time = resp.elapsed.microseconds / math.pow(10, 6)
+    return use_time
---
+++ Server/server.py
@@ -0,0 +1,35 @@
+#encoding:utf-8
+import json
+import logging
+import traceback
+
+import requests
+from flask import Flask,render_template,Response,request
+
+from Server.Service import fanqiang_ip_service
+from Utils import  log_utils
+
+
+app = Flask(__name__,static_folder='static',template_folder='dist')
+app.debug=True
+logger = log_utils.Log('Server/log/server',logging.DEBUG if app.debug else logging.ERROR).logger
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/get_new_ip_port',methods=['GET'])
+def get_lans_data():
+    request_id = request.args['uuid']
+    # 获取能用的ip_with_port，
+    try:
+        ip_with_port=fanqiang_ip_service.get_random_ip_port_dict()
+    except Exception as e:
+        logger.error("%s - %s" % (request_id,traceback.format_exc()))
+        return Response("服务器出错，请联系管理员，请求id是：%s" % request_id,status=500)
+    return Response(ip_with_port['ip_with_port'])
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5083')
